93e07ee086239b6a07a161b6f0b3a4725f802e90
Joli24 <151483310+Joli24@users.noreply.github.com>
2024-08-25 11:57:01 -0500

    Add files via upload

---
+++ js09b_txtpg367.js
@@ -0,0 +1,16 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+      
+      Eating Well in Season 
+      Author: 
+      Date:   
+      
+      Filename: js09b.js
+ */
+ 
+ 
+// Retrieve the text of the query string
+let qString = location.search.slice(1);
+console.log(qString);
